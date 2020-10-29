7ff06ab9aac0bde6473604bbe2cf890cb9865642
jakesheinberg <23jsheinberg@athenian.org>
2020-10-29 15:36:40 +0000

    final copy done

--- my_code.py
+++ my_code.py
@@ -13,14 +13,13 @@ def mt_tik_tak_toe(locations):
 mt_tik_tak_toe(locations)#prints initial blank board
 x=0
 while x<=9:
-    if locations['a1']==locations['a2']==locations['a3']!=' ' or locations['b1']==locations['b2']==locations['b3']!=' ' or locations['c1']==locations['c2']==locations['c3']!=' ' or locations['a1']==locations['b1']==locations['c1']!=' 'or locations['a2']==locations['b2']==locations['c2']!=' ' or locations['a3']==locations['b3']==locations['c3']!=' ' or locations['a1']==locations['b2']==locations['c3']!=' ' or locations['a3']==locations['b2']==locations['c1']!=' ':
-            mt_tik_tak_toe(locations)#prints final status of game
+    if locations['a1']==locations['a2']==locations['a3']!=' ' or locations['b1']==locations['b2']==locations['b3']!=' ' or locations['c1']==locations['c2']==locations['c3']!=' ' or locations['a1']==locations['b1']==locations['c1']!=' 'or locations['a2']==locations['b2']==locations['c2']!=' ' or locations['a3']==locations['b3']==locations['c3']!=' ' or locations['a1']==locations['b2']==locations['c3']!=' ' or locations['a3']==locations['b2']==locations['c1']!=' ':#allwins
             print("Game Over!")
             break
     elif x==9:
             print("Its a tie")
             mt_tik_tak_toe
-            break
+            break #measures if all boxes are filled
     else:
             spot=input("select the square you would like to play: ")
             if x%2==0:
